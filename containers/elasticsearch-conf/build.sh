5ad29364addfe47c414e467d32f1f30656462ba4
Marc Melvin <mmelvin@smartprocure.us>
2016-07-13 13:14:56 -0700

    Trying it again.

--- containers/elasticsearch-conf/build.sh
+++ containers/elasticsearch-conf/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t registry.smartprocure.us/rancher/elasticsearch-conf:v1 .
-docker push registry.smartprocure.us/rancher/elasticsearch-conf:v1
+docker build -t registry.smartprocure.us/rancher/elasticsearch-conf:v2 .
+docker push registry.smartprocure.us/rancher/elasticsearch-conf:v2
--- containers/elasticsearch-conf/run.sh
+++ containers/elasticsearch-conf/run.sh
@@ -2,20 +2,12 @@
 
 set -e
 
-# PLUGIN_TXT=${PLUGIN_TXT:-/usr/share/elasticsearch/plugins.txt}
-
 while [ ! -f "/usr/share/elasticsearch/config/elasticsearch.yml" ]; do
     sleep 1
 done
 
-# if [ -f "$PLUGIN_TXT" ]; then
-#     for plugin in $(<"${PLUGIN_TXT}"); do
-#         /usr/share/elasticsearch/bin/plugin --install $plugin
-#     done
-# # else
-    for plugin in $(echo $PLUGIN_TXT | tr "," "\n"); do
-        /usr/share/elasticsearch/bin/plugin --install $plugin
-    done
-# fi
+for plugin in $(echo $PLUGIN_TXT | tr "," "\n"); do
+    /usr/share/elasticsearch/bin/plugin install $plugin
+done
 
 exec /docker-entrypoint.sh elasticsearch
